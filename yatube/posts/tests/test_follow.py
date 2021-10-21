d41dc7019c8143883d2708a4ba304676a1fbd66a
Treymf08 <treym08@yandex.ru>
2021-10-22 00:00:44 +0300

    6 sprint after revue

--- yatube/posts/tests/test_follow.py
+++ yatube/posts/tests/test_follow.py
@@ -22,11 +22,12 @@ class PostsPagesTests(TestCase):
         self.authorized_client.force_login(self.user)
         self.authorized_client_follow.force_login(self.user_follow)
 
-    def test_profile_unfollow(self):
+    def test_profile_follow(self):
         """Проверяем, что пользователь может
-        подписываться и отписываться от других пользователей.
+        подписываться на других пользователей.
         """
         count_follows = Follow.objects.count()
+
         self.authorized_client.get(
             reverse('posts:profile_follow', kwargs={
                     'username': 'TestFollow'})
@@ -35,34 +36,53 @@ class PostsPagesTests(TestCase):
 
         self.assertEqual(count_follows_after_cread, count_follows + 1)
 
+    def test_profile_unfollow(self):
+        """Проверяем, что пользователь может
+        отписываться от других пользователей.
+        """
+        self.authorized_client.get(
+            reverse('posts:profile_follow', kwargs={
+                    'username': 'TestFollow'})
+        )
+        count_follows = Follow.objects.count()
         self.authorized_client.get(
             reverse('posts:profile_unfollow', kwargs={
                     'username': 'TestFollow'})
         )
         count_follows_after_delete = Follow.objects.count()
 
-        self.assertEqual(count_follows_after_delete, count_follows)
+        self.assertEqual(count_follows_after_delete, count_follows - 1)
 
-    def test_new_post_in_follow_(self):
+    def test_new_post_in_follow(self):
         """Проверяем, что новая запись пользователя
-        появляется в ленте тех, кто на него подписан
-        и не появляется в ленте тех, кто не подписан."""
-        Follow.objects.create(user=self.user, author=self.user_follow)
+        появляется в ленте тех, кто на него подписан.
+        """
+        Follow.objects.create(user=self.user_follow, author=self.user)
         Post.objects.create(
-            author=self.user_follow,
+            author=self.user,
             text='Test'
         )
 
-        response = self.authorized_client.get(reverse(
+        response = self.authorized_client_follow.get(reverse(
             'post:follow_index')
         )
         count_post = len(response.context['page_obj'])
 
         self.assertEqual(count_post, 1)
 
-        response_not_follow = self.authorized_client_follow.get(reverse(
+    def test_new_post_in_unfollow(self):
+        """Проверяем, что новая запись пользователя
+         не появляется в ленте тех, кто на него не подписан.
+        """
+        Follow.objects.create(user=self.user_follow, author=self.user)
+        Post.objects.create(
+            author=self.user,
+            text='Test'
+        )
+
+        response_not_follow = self.authorized_client.get(reverse(
             'post:follow_index')
         )
-        count_post_not_follow = len(response_not_follow.context['page_obj'])
+        count_post = len(response_not_follow.context['page_obj'])
 
-        self.assertEqual(count_post_not_follow, 0)
+        self.assertEqual(count_post, 0)
